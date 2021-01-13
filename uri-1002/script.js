e3a47a4791bd1b41a8d92b5e85eaeafbbd8b7d15
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2021-01-13 11:46:00 -0300

    added uri-1002 answer

---
+++ uri-1002/script.js
@@ -0,0 +1,19 @@
+const input = require( 'fs' ).readFileSync( 'stdin', 'utf-8' );
+
+/**
+* A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+Entrada
+A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+Saída
+Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+*/
+
+const raio = parseFloat( input );
+const pi = 3.14159;
+
+const area = pi * ( raio * raio );
+
+console.log('A=' + area.toFixed( 4 ) );
+
+
