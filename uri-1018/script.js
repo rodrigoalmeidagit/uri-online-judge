8a51891d1ad87fd79f9da1c3bf32a44c91fb0fa3
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2021-01-17 02:27:51 -0300

    added uri-1018 answer

---
+++ uri-1018/script.js
@@ -0,0 +1,29 @@
+const { parse } = require('path');
+
+const input = require( 'fs' ).readFileSync( 'stdin', 'utf-8' );
+/*
+Leia um valor inteiro. A seguir, calcule o menor número de notas possíveis (cédulas) no qual o valor pode ser decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. A seguir mostre o valor lido e a relação de notas necessárias.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N (0 < N < 1000000).
+
+Saída
+Imprima o valor lido e, em seguida, a quantidade mínima de notas de cada tipo necessárias, conforme o exemplo fornecido. Não esqueça de imprimir o fim de linha após cada linha, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+576
+5 nota(s) de R$ 100,00
+1 nota(s) de R$ 50,00
+1 nota(s) de R$ 20,00
+0 nota(s) de R$ 10,00
+1 nota(s) de R$ 5,00
+0 nota(s) de R$ 2,00
+1 nota(s) de R$ 1,00
+*/
+var valor = parseInt( input );
+var notas = [ 100, 50, 20, 10, 5, 2, 1 ];
+
+console.log( valor );
+for( var nota of notas ) {
+    var qtNotas = parseInt( valor / nota );
+    console.log( `${ qtNotas } nota(s) de R$ ${ nota },00` );
+    valor = valor % nota;
+}
